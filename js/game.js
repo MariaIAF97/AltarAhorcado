6041ae35a73f8627832e632c3fc5ef7b0fd89f7d
SergioRodriguez317 <sergioarg17@gmail.com>
2018-08-30 22:45:14 -0500

    Validaciones parciales

--- js/game.js
+++ js/game.js
@@ -29,3 +29,31 @@ function choseTopic(e){
     chosenTopic=topics[2][Math.floor(Math.random()*topics[2].length)];
   }
 }
+
+//Cambios checho (Borrar esto)
+var palabras = [["atlantico", "Un océano"], ["ordenador", "Una máquina"], ["laurel", "Un árbol"], ["plaza", "Espacio público"], ["rueda", "Gran invento"], ["cereza", "Una fruta"], ["petanca", "Un juego"], ["higuera", "Un árbol"], ["everest", "Un monte"], ["relampago", "Antecede al trueno"], ["jirafa", "Un animal"], ["luxemburgo", "Un país"], ["uruguay", "Un país"], ["ilustracion", "Representación gráfica"], ["excursion", "Actividad en la naturaleza"], ["empanadilla", "De la panadería"], ["pastel", "De la pastelería"], ["colegio", "Lugar para estudiar"], ["carrera", "Competición"], ["mermelada", "Confitura"]];
+var palabra = "";
+var rand;
+var oculta = [];
+
+function inicio() {
+    generaPalabra();
+  }
+
+  function generaPalabra() {
+    rand = (Math.random() * 19).toFixed(0);
+    palabra = palabras[rand][0].toUpperCase();
+    console.log(palabra);
+  }
+
+  function intento(letra) {
+    document.getElementById(letra).disabled = true;
+    if(palabra.indexOf(letra) != -1) {
+      for(var i=0; i<palabra.length; i++) {
+        if(palabra[i]==letra) oculta[i] = letra;
+      }
+      console.log("Acierto");
+    }else{
+      console.log("Fallo")
+    }
+  }
